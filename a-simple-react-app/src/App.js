713accce29141f51a16a0b23317ca6f09e4dd9ff
ZohirRayhan <rayhanzohir925@gmail.com>
2018-06-25 19:04:53 +0300

    clear uped the wroking file

--- a-simple-react-app/src/App.js
+++ a-simple-react-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Main Container ... in the main component all the the sub component gonna
+// puted be ..
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello Hello </h1>
+        hello
       </div>
     );
   }
